9bd6a975da6d7c97625499c1a3fae830d10cda99
DanielQ19 <daniel.quinones@ingeniocontenido.co>
2018-04-26 13:59:41 -0500

    Holograma
    creacion de holograma

---
+++ hardhelmets/Assets/Script/holograma.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holograma : MonoBehaviour {
+
+	public float ye;//-27.92f
+	public float zeta;//-181.7f
+
+	public GameObject hologram;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Hero")
+		{
+			Instantiate(hologram, new Vector3(col.transform.position.x, ye, zeta), Quaternion.Euler(0,0,0));
+		}
+	}
+}
